1ea2a053ade631bff7ba24064038a9bd2122f6f9
沈张一 <3256152226@qq.com>
2026-04-24 17:25:51 +0800

    关于this关键字

--- src/plan/attack/ooptest2/Student.java
+++ src/plan/attack/ooptest2/Student.java
@@ -7,17 +7,17 @@ public class Student {
     public void setName(String str) {
         name = str;
     }
-    public void setAge(int n) {
-        if (n > 0 && n < 120) {
-            age = n;
+    public void setAge(int age) {
+        if (age > 0 && age < 120) {
+            this.age = age;
         }
         else {
             System.out.println("年龄不合法");
         }
     }
-    public void setScore(double s) {
-        if (s >= 0 && s <= 150) {
-            score = s;
+    public void setScore(double score) {
+        if (score >= 0 && score <= 150) {
+            this.score = score;
         }
         else {
             System.out.println("分数不合法");
